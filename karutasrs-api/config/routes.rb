6f5132d6b0e350182427d93ee394fab38659f2c3
GoldenChrysus <GoldenChrysus@users.noreply.github.com>
2019-11-28 15:19:27 -0500

    Disable routes that manipulate poems

--- karutasrs-api/config/routes.rb
+++ karutasrs-api/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 	namespace "api" do
 		jsonapi_resources :users
-		jsonapi_resources :poems
+		jsonapi_resources :poems, only: [:index, :show]
 		jsonapi_resources :learned_items
 		# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 	end
--- karutasrs-api/spec/routing/poems_routing_spec.rb
+++ karutasrs-api/spec/routing/poems_routing_spec.rb
@@ -14,19 +14,19 @@ RSpec.describe Api::PoemsController, type: :routing do
 
 
     it "routes to #create" do
-      expect(:post => "#{api_path}/poems").to route_to("api/poems#create")
+      expect(:post => "#{api_path}/poems").not_to be_routable
     end
 
     it "routes to #update via PUT" do
-      expect(:put => "#{api_path}/poems/1").to route_to("api/poems#update", :id => "1")
+      expect(:put => "#{api_path}/poems/1").not_to be_routable
     end
 
     it "routes to #update via PATCH" do
-      expect(:patch => "#{api_path}/poems/1").to route_to("api/poems#update", :id => "1")
+      expect(:patch => "#{api_path}/poems/1").not_to be_routable
     end
 
     it "routes to #destroy" do
-      expect(:delete => "#{api_path}/poems/1").to route_to("api/poems#destroy", :id => "1")
+      expect(:delete => "#{api_path}/poems/1").not_to be_routable
     end
   end
 end
