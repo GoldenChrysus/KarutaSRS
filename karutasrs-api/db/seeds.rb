d6815bbbe69d9f7be869585d0f27caefeb49c36c
GoldenChrysus <GoldenChrysus@users.noreply.github.com>
2019-11-30 02:38:56 -0500

    Remove excess whitespace

--- karutasrs-api/db/seeds.rb
+++ karutasrs-api/db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-poems = Poem.create([	
+poems = Poem.create([
 	{
 		id: "1",
 		name: "Tenchi Tenno",
