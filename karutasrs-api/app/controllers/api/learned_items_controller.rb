2d79400dbc091084bc6962600cdb8719c48a91e9
Patrick Golden <GoldenChrysus@users.noreply.github.com>
2019-12-02 14:10:14 -0500

    Add missing variables to controller

--- karutasrs-api/app/controllers/api/learned_items_controller.rb
+++ karutasrs-api/app/controllers/api/learned_items_controller.rb
@@ -1,6 +1,8 @@
 module Api
 	class LearnedItemsController < ApplicationController
 		def complete_review
+			item_id       = params[:id]
+			parsed_data   = JSON.parse(request.raw_post)
 			wrong_answers = parsed_data["wrong_answers"]
 			item          = LearnedItem.find(item_id)
 			result        = item.complete_review(wrong_answers)
