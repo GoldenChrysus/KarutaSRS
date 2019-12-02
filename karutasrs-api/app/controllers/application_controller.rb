e3b66836821388a16059182695215228c29d4f1e
Patrick Golden <GoldenChrysus@users.noreply.github.com>
2019-12-02 15:17:54 -0500

    Try some generic error handling

--- karutasrs-api/app/controllers/application_controller.rb
+++ karutasrs-api/app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < JSONAPI::ResourceController
 	include Pundit
 
 	protect_from_forgery with: :null_session
+
+	rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+	rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+	def render_unprocessable_entity_response(exception)
+		render json: exception.record.errors, status: :unprocessable_entity
+	end
+
+	def render_not_found_response(exception)
+		render json: { error: exception.message }, status: :not_found
+	end
 end
