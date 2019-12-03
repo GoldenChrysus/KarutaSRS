7ce934cbaf4efe5950e79075cdc44c70d702ebdc
GoldenChrysus <GoldenChrysus@users.noreply.github.com>
2019-12-02 19:44:00 -0500

    Consolidate some error handling

--- karutasrs-api/app/controllers/application_controller.rb
+++ karutasrs-api/app/controllers/application_controller.rb
@@ -4,7 +4,6 @@ class ApplicationController < JSONAPI::ResourceController
 	protect_from_forgery with: :null_session
 
 	rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
-	rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 	rescue_from ApiErrors::BaseError, with: :render_api_error
 	rescue_from StandardError, with: :handle_standard_error
 
@@ -12,24 +11,19 @@ class ApplicationController < JSONAPI::ResourceController
 		render json: exception.record.errors, status: :unprocessable_entity
 	end
 
-	def render_not_found_response(exception)
-		data = {
-			:title  => "Record not found",
-			:detail => "The #{exception.model} identified by #{exception.id} could not be found",
-			:code   => 404,
-			:status => 404
-		}
-
-		render json: { errors: Array.new.push(data) }, status: :not_found
-	end
-
 	def render_api_error(exception)
 		render json: { errors: Array.new.push(exception.data) }, status: exception.code
 	end
 
 	def handle_standard_error(exception)
-		if (exception.class.to_s[0..8] === "ApiErrors")
+		class_name = exception.class.to_s
+
+		if (class_name[0..8] === "ApiErrors")
 			self.render_api_error(exception)
+		elsif (class_name === "ActiveRecord::RecordNotFound")
+			error = ApiErrors::BaseError.new("Record not found", "The #{exception.model} identified by #{exception.id} could not be found", 404)
+
+			self.render_api_error(error)
 		else
 			error = ApiErrors::BaseError.new("Server error", exception.message, 500)
 
