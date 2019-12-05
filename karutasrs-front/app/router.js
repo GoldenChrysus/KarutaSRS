ff97f5e802b7308f3bcafebf3093b8e2a2f1a409
GoldenChrysus <GoldenChrysus@users.noreply.github.com>
2019-12-04 23:46:21 -0500

    Typo

--- karutasrs-front/app/router.js
+++ karutasrs-front/app/router.js
@@ -10,7 +10,7 @@ Router.map(function() {
 	this.route("login");
 	this.route("authenticated", { path : "/" }, function() {
 		// Index is not an authenticated route. However, having it at "/" and also having the authenticated routes at "/" causes issues with doing the authenticated redirects.
-		// We could fix this by either not namespacing the authenticated routes or by nesting the index route here. The latter was chosen. This way, authenticated routes are clear identifiable
+		// We could fix this by either not namespacing the authenticated routes or by nesting the index route here. The latter was chosen. This way, authenticated routes are clearly identifiable
 		// as such in the code, e.g. "authenticated.dashboard" and we only have one special route (index) that is nested here as an unauthenticated route. It can be referenced in the code as "index".
 		this.route("index", {path : "/", resetNamespace : true});
 
