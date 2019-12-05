02bb60831d7473a55dbcb1a2b796eae81b595362
GoldenChrysus <GoldenChrysus@users.noreply.github.com>
2019-12-05 18:12:59 -0500

    Remove root Rails route.

--- karutasrs-api/config/routes.rb
+++ karutasrs-api/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+	root to: proc { [404, {}, ["Not found."]] }
+
 	namespace "api" do
 		get "users/:id/review-queue", to: "users#review_queue"
 		get "users/:id/lesson-queue", to: "users#lesson_queue"
