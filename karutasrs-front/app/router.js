28dc4540eb64204768b0be0c9df87e7d444673d4
GoldenChrysus <GoldenChrysus@users.noreply.github.com>
2019-12-04 23:45:52 -0500

    Explain index route nesting

--- karutasrs-front/app/router.js
+++ karutasrs-front/app/router.js
@@ -9,7 +9,11 @@ export default class Router extends EmberRouter {
 Router.map(function() {
 	this.route("login");
 	this.route("authenticated", { path : "/" }, function() {
+		// Index is not an authenticated route. However, having it at "/" and also having the authenticated routes at "/" causes issues with doing the authenticated redirects.
+		// We could fix this by either not namespacing the authenticated routes or by nesting the index route here. The latter was chosen. This way, authenticated routes are clear identifiable
+		// as such in the code, e.g. "authenticated.dashboard" and we only have one special route (index) that is nested here as an unauthenticated route. It can be referenced in the code as "index".
 		this.route("index", {path : "/", resetNamespace : true});
+
 		this.route("dashboard");
 	});
 });
