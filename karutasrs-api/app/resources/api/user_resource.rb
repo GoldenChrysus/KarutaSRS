7996fd0b782e2893c5f9751b219c22d19cf3f329
GoldenChrysus <GoldenChrysus@users.noreply.github.com>
2019-12-05 22:16:49 -0500

    Ensure password (even hashed) isn't fetchable on API

--- karutasrs-api/app/resources/api/user_resource.rb
+++ karutasrs-api/app/resources/api/user_resource.rb
@@ -6,6 +6,10 @@ module Api
 
 		filters :email
 
+		def fetchable_fields
+			super - [:password]
+		end
+
 		# Attribute controls
 		def self.updatable_fields(context)
 			# Bearer may never be updated
