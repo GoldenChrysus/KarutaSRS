dba60d06e65ac89decc6947a9fc445bca2c8e228
GoldenChrysus <GoldenChrysus@users.noreply.github.com>
2019-12-07 17:09:02 -0500

    Prepare for lesson tracking

--- karutasrs-front/app/components/lesson-carousel.js
+++ karutasrs-front/app/components/lesson-carousel.js
@@ -1,6 +1,9 @@
 import Component from '@glimmer/component';
+import { tracked } from "@glimmer/tracking";
 
 export default class LessonCarouselComponent extends Component {
+	@tracked current_lesson = 0;
+
 	queue            = this.args.queue;
 	multiple_lessons = (this.args.queue.length > 1);
 }
