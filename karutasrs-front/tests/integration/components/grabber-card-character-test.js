e5edd320e25ac6d059b10bc469e2981de8defa95
GoldenChrysus <GoldenChrysus@users.noreply.github.com>
2019-12-07 00:04:32 -0500

    Rename grabber card character component to avoid redundancy

--- karutasrs-front/tests/integration/components/grabber-card-character-test.js
+++ karutasrs-front/tests/integration/components/grabber-card-character-test.js
@@ -3,22 +3,22 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
-module('Integration | Component | grabber-card-character', function(hooks) {
+module('Integration | Component | grabber-card::character', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`<GrabberCardCharacter />`);
+    await render(hbs`<GrabberCard::Character />`);
 
     assert.equal(this.element.textContent.trim(), '');
 
     // Template block usage:
     await render(hbs`
-      <GrabberCardCharacter>
+      <GrabberCard::Character>
         template block text
-      </GrabberCardCharacter>
+      </GrabberCard::Character>
     `);
 
     assert.equal(this.element.textContent.trim(), 'template block text');
