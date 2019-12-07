fa0015a4a74dfdf5d998ac37af46d492e4db2cf0
GoldenChrysus <GoldenChrysus@users.noreply.github.com>
2019-12-07 00:05:42 -0500

    Syntax consistency

--- karutasrs-front/tests/integration/components/grabber-card-character-test.js
+++ karutasrs-front/tests/integration/components/grabber-card-character-test.js
@@ -3,7 +3,7 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
-module('Integration | Component | grabber-card::character', function(hooks) {
+module('Integration | Component | grabber-card/character', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
