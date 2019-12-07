880c8cf69b5fd270c8b54a19982626715063aa0a
GoldenChrysus <GoldenChrysus@users.noreply.github.com>
2019-12-06 23:17:36 -0500

    Add fade-in when grabbar character value is changed

--- karutasrs-front/app/components/grabber-card/grabber-card-character.js
+++ karutasrs-front/app/components/grabber-card/grabber-card-character.js
@@ -2,7 +2,7 @@ import Component from '@glimmer/component';
 import { tracked } from "@glimmer/tracking";
 
 export default class GrabberCardCharacterComponent extends Component {
-	@tracked char = this.args.char || "&nbsp;";
+	@tracked char = this.args.char;
 
 	is_answer   = this.args.is_answer;
 	is_learning = (this.args.type === "learn");
@@ -12,6 +12,10 @@ export default class GrabberCardCharacterComponent extends Component {
 
 		if (this.is_answer && this.is_learning) {
 			classes.push("learn");
+		}
+
+		if (this.char) {
+			classes.push("active");
 		}
 
 		return classes.join(" ");
