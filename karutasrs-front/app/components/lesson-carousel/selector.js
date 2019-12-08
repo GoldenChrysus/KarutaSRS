550a2b22fd7c98e6b3bf7586435ad97bf96d44eb
GoldenChrysus <GoldenChrysus@users.noreply.github.com>
2019-12-07 19:57:04 -0500

    Add carousel completion tracking

--- karutasrs-front/app/components/lesson-carousel/selector.js
+++ karutasrs-front/app/components/lesson-carousel/selector.js
@@ -5,12 +5,32 @@ import { action } from "@ember/object";
 export default class LessonCarouselSelectorComponent extends Component {
 	@tracked current_lesson = 0;
 
-	queue = this.args.queue;
+	@tracked queue = this.args.queue;
+
+	completed = [];
+
+	@action
+	didInsert() {
+		this.updateCompletion(this.current_lesson);
+	}
 
 	@action
 	changeLesson(index) {
 		this.current_lesson = index;
 
+		this.updateCompletion(index);
 		this.args.onChange(index);
+	}
+
+	updateCompletion(index) {
+		let queue = JSON.parse(JSON.stringify(this.queue));
+		
+		queue[index].completed = true;
+
+		this.queue = queue;
+
+		if (!this.completed.includes(index)) {
+			this.completed.push(index);
+		}
 	}
 }
