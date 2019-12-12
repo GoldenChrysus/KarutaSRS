7c1fa43b212fddd209c68158ea7ed2f96565fc54
GoldenChrysus <GoldenChrysus@users.noreply.github.com>
2019-12-12 00:06:13 -0500

    Fix sidebar label coloring

--- karutasrs-front/app/routes/application.js
+++ karutasrs-front/app/routes/application.js
@@ -14,6 +14,7 @@ export default Route.extend(ApplicationRouteMixin, {
 
 	async model() {
 		this.prefilter.injectBearer();
+		await this.user.getUser();
 
 		return {};
 	},
--- karutasrs-front/app/routes/authenticated/lessons/index.js
+++ karutasrs-front/app/routes/authenticated/lessons/index.js
@@ -1,5 +1,16 @@
 import Route from '@ember/routing/route';
 import AuthenticatedRouteMixin from "ember-simple-auth/mixins/authenticated-route-mixin";
+import { inject as service } from "@ember/service";
 
 export default Route.extend(AuthenticatedRouteMixin, {
+	user_serv : service("current-user"),
+
+	async model() {
+		let user  = this.user_serv.peekUser();
+		let poems = await user.lesson_queue;
+
+		return {
+			poems : poems.data
+		}
+	}
 });
