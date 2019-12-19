28ecac1ab41dafc86415ac1e20a51d8353a65df4
GoldenChrysus <GoldenChrysus@users.noreply.github.com>
2019-12-18 21:34:37 -0500

    Prep API for SSL serving on production domain

--- karutasrs-api/config/environments/production.rb
+++ karutasrs-api/config/environments/production.rb
@@ -44,7 +44,10 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
+
+  # Allow domain to host
+  config.hosts << ENV["frontend_address"]
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
