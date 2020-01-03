b50b6e998f53483ae8a77ca5c32c8c0d339df4fa
Adrien Pyke <adrien.pyke@034gc.onmicrosoft.com>
2020-01-03 15:45:06 -0500

    filter by poem length

--- karutasrs-front/app/components/filtered-poem-grid.js
+++ karutasrs-front/app/components/filtered-poem-grid.js
@@ -5,9 +5,16 @@ import { sortBy } from '../common/util';
 
 export default class FilteredPoemGridComponent extends Component {
 	@tracked sort = 'num';
+	@tracked kimariji_length;
+	@tracked verse2_length;
 
 	get poems() {
-		return this.args.poems.toArray().sort(
+		let poems = this.args.poems.toArray();
+		if (this.kimariji_length)
+			poems = poems.filter(p => [...p.kimariji].length === this.kimariji_length);
+		if (this.verse2_length)
+		  poems = poems.filter(p => [...p.second_verse_answer].length === this.verse2_length);
+		return poems.sort(
 			sortBy(
 				p =>
 					this.sort === 'kimariji'
@@ -21,7 +28,9 @@ export default class FilteredPoemGridComponent extends Component {
 	}
 
 	@action
-	onChange({ sort }) {
+	onChange({ sort, kimariji_length, verse2_length }) {
 		this.sort = sort;
+		this.kimariji_length = kimariji_length;
+		this.verse2_length = verse2_length;
 	}
 }
--- karutasrs-front/app/components/poem-filters.js
+++ karutasrs-front/app/components/poem-filters.js
@@ -3,11 +3,35 @@ import { action } from '@ember/object';
 
 export default class PoemFiltersComponent extends Component {
 	get sort() {
-		return this.args.sort
+		return this.args.sort;
 	}
 
 	set sort(sort) {
-		this.args.onChange && this.args.onChange({ sort })
+		this.onChange({ ...this.filters, sort });
+	}
+
+	get kimariji_length() {
+		return this.args.kimariji_length;
+	}
+
+	set kimariji_length(kimariji_length) {
+		this.onChange({ ...this.filters, kimariji_length: Number(kimariji_length) });
+	}
+
+	get verse2_length() {
+		return this.args.verse2_length;
+	}
+
+	set verse2_length(verse2_length) {
+		this.onChange({ ...this.filters, verse2_length: Number(verse2_length) })
+	}
+
+	get filters() {
+		return { sort: this.sort, kimariji_length: this.kimariji_length, verse2_length: this.verse2_length };
+	}
+
+	@action onChange(filters) {
+		this.args.onChange && this.args.onChange(filters);
 	}
 
 	@action
