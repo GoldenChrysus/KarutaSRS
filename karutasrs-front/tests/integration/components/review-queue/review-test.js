0734974f3f76b131f8563fd24b860cde46cc2bcd
GoldenChrysus <GoldenChrysus@users.noreply.github.com>
2020-01-04 20:24:04 -0500

    More attempted test fixes

--- karutasrs-front/tests/integration/components/review-queue/review-test.js
+++ karutasrs-front/tests/integration/components/review-queue/review-test.js
@@ -6,17 +6,9 @@ import { hbs } from 'ember-cli-htmlbars';
 module('Integration | Component | review-queue/review', function(hooks) {
 	setupRenderingTest(hooks);
 
-	test('it renders', async function(assert) {
+	test('it throws an error when rendered without data', async function(assert) {
 		// Set any properties with this.set('myProperty', 'value');
 		// Handle any actions with this.set('myAction', function(val) { ... });
-		let error = "";
-
-		try {
-			await render(hbs`<ReviewQueue::Review />`)
-		} catch (e) {
-			error = e.message;
-		}
-			
-		assert.equal(error.slice(0, 7), "No poem");
+		assert.rejects(render(hbs`<ReviewQueue::Review />`));
 	});
 });
