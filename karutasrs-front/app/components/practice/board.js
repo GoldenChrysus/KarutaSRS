cedcb80310e56f07f18236eed75d9c8e23aafee5
GoldenChrysus <17366720+GoldenChrysus@users.noreply.github.com>
2021-03-11 13:51:38 +0900

    Improve how board card list is structured

--- karutasrs-front/app/components/practice/board.js
+++ karutasrs-front/app/components/practice/board.js
@@ -1,48 +1,50 @@
 import Component from "@ember/component";
+import { computed } from "@ember/object";
 
 export default Component.extend({
-	rows : [
-		[
-			null,
-			null,
-			null,
-			null,
-			null,
-			null,
-			null,
-			null,
-			null,
-			null,
-			null,
-			null
-		],
-		[
-			null,
-			null,
-			null,
-			null,
-			null,
-			null,
-			null,
-			null,
-			null,
-			null,
-			null,
-			null
-		],
-		[
-			null,
-			null,
-			null,
-			null,
-			null,
-			null,
-			null,
-			null,
-			null,
-			null,
-			null,
-			null
-		]
-	]
+	cards : [
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null,
+		null
+	],
+	rows : computed("cards", function() {
+		return [
+			this.cards.slice(0, 11),
+			this.cards.slice(12, 23),
+			this.cards.slice(24, 35)
+		];
+	})
 });
